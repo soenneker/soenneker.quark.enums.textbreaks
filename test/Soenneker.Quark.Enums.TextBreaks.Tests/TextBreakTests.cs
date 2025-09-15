0dffd0ad225120144687bb775f0a2805e4ca504c
Jake Soenneker <jake@soenneker.com>
2025-09-15 17:03:02 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Enums.TextBreaks.Tests/TextBreakTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.TextBreaks.Tests;
+
+public sealed class TextBreakTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
