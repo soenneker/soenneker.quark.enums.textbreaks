c5b5c5b93e687403bc4743fa0164f5a8c0efe775
Jake Soenneker <jake@soeneker.com>
2026-02-26 08:49:50 -0600

    Enum

--- src/TextBreakKeyword.cs
+++ src/TextBreakKeyword.cs
@@ -1,11 +1,11 @@
-using Intellenum;
+using Soenneker.Gen.EnumValues;
 
 namespace Soenneker.Quark.Enums;
 
 /// <summary>
 /// An HTML CSS keyword set in .NET enumeration form
 /// </summary>
-[Intellenum<string>]
+[EnumValue<string>]
 public partial class TextBreakKeyword
 {
     public static readonly TextBreakKeyword Enable = new("enable");
