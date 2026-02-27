fb66bc25505e76788b1e50234612282e3cf41a0d
Jake Soenneker <jake@soenneker.com>
2026-02-27 08:22:55 -0600

    Deps

--- src/TextBreakKeyword.cs
+++ src/TextBreakKeyword.cs
@@ -6,10 +6,9 @@ namespace Soenneker.Quark.Enums;
 /// An HTML CSS keyword set in .NET enumeration form
 /// </summary>
 [EnumValue<string>]
+[IncludeEnumValues(typeof(GlobalKeyword))]
 public partial class TextBreakKeyword
 {
     public static readonly TextBreakKeyword Enable = new("enable");
     public static readonly TextBreakKeyword Disable = new("disable");
-
-    public static implicit operator TextBreakKeyword(GlobalKeyword style) => new(style.Value); // Value is the underlying string
 }
